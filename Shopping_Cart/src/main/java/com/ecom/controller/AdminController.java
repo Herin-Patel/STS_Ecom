f34875dd8e26b1d01e4519f4038e20069f062f4f
Herin Patel <herin.patel@volansys.com>
2026-03-10 12:35:39 +0530

    Started with backend logic of Add Category of Product in Admin page

--- Shopping_Cart/src/main/java/com/ecom/controller/AdminController.java
+++ Shopping_Cart/src/main/java/com/ecom/controller/AdminController.java
@@ -1,26 +1,59 @@
 package com.ecom.controller;
 
+import com.ecom.model.Category;
+import com.ecom.service.CategoryService;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+
+import org.springframework.util.ObjectUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	
+
+	@Autowired
+	private CategoryService categoryServiceObj;
+
 	@GetMapping("/")
 	public String index() {
 		return "admin/index";
 	}
-	
+
 	@GetMapping("/loadAddProduct")
 	public String loadAddProduct() {
 		return "admin/add_product";
 	}
-	
+
 	@GetMapping("/category")
 	public String category() {
 		return "admin/category";
+	}
+
+	@PostMapping("/saveCategory")
+	public String saveCategory(@ModelAttribute Category currentCategory, HttpSession session) {
+		
+		if (categoryServiceObj.existCategory(currentCategory.getName())) {
+			session.setAttribute("errorMssg", "Category name already exists !");
+		} else {
+			Category savedCategory = categoryServiceObj.saveCategory(currentCategory);
+			
+			if (ObjectUtils.isEmpty(savedCategory)) {
+				session.setAttribute("errorMssg", "Not saved ! Internal server error.");
+			} else {
+				session.setAttribute("successMssg","Category saved successfully.");
+			}
+		}
+		
+		return "redirect:/admin/category";
 	}
 }
---
+++ Shopping_Cart/src/main/java/com/ecom/model/Category.java
@@ -0,0 +1,30 @@
+package com.ecom.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String imageName;
+	private Boolean isActive;
+
+	public String getName() {
+		return this.name;
+	}
+}
