9332c59fb7c9b7b84aeb21e32b22b8e3f72daec4
Herin Patel <herin.patel@volansys.com>
2026-07-02 20:21:04 +0530

    Working on Email notification for Product Order

--- Shopping_Cart/src/main/java/com/ecom/util/CommonUtil.java
+++ Shopping_Cart/src/main/java/com/ecom/util/CommonUtil.java
@@ -7,6 +7,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import com.ecom.model.ProductOrder;
+
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,13 +24,14 @@ public class CommonUtil {
 		MimeMessage message = mailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message);
 
-		helper.setFrom("herrypatel7290@gmail.com", "Shopping Cart");
-		helper.setTo(recipientMail);
-
-		String content = "<p>Hello, </p>" + "<p>You have requested to reset your password. </p>"
-				+ "<p>Click the link below to change your password : </p>" + "<p><a href=\"" + url
+		String content = "<p>Hello, </p>" 
+				+ "<p>You have requested to reset your password. </p>"
+				+ "<p>Click the link below to change your password : </p>" 
+				+ "<p><a href=\"" + url
 				+ "\">Change my password</a></p>";
 
+		helper.setFrom("herrypatel7290@gmail.com", "Shopping Cart");
+		helper.setTo(recipientMail);
 		helper.setSubject("Password Reset");
 		helper.setText(content, true);
 		mailSender.send(message);
@@ -42,5 +45,33 @@ public class CommonUtil {
 		String siteUrl = request.getRequestURL().toString();
 
 		return siteUrl.replace(request.getServletPath(), "");
+	}
+	
+	String orderContent = "<p>Thank you, your order has been placed successfully.</p>"
+				  + "<p>Product Details :- </p>"
+				  + "<p>Name : [[productName]] </p>"
+				  + "<p>Category : [[category]] </p>"
+				  + "<p>Quantity : [[quantity]] </p>"
+				  + "<p>Price : [[price]] </p>"
+				  + "<p>Payment Type : [[paymentType]] </p>";
+			
+
+	public Boolean sendMailForProductOrder(ProductOrder order) throws Exception, MessagingException {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+
+		orderContent = orderContent.replace("[[productName]]", order.getProduct().getTitle());
+		orderContent = orderContent.replace("[[category]]", order.getProduct().getCategory());
+		orderContent = orderContent.replace("[[quantity]]", Integer.toString(order.getQuantity()));
+		orderContent = orderContent.replace("[[price]]", order.getPrice().toString());
+		orderContent = orderContent.replace("[[paymentType]]", order.getPaymentType());
+
+		helper.setFrom("herrypatel7290@gmail.com", "Shopping Cart");
+		helper.setTo(order.getOrderAddress().getEmail());
+		helper.setSubject("Product Order Status");
+		helper.setText(orderContent, true);
+		mailSender.send(message);
+
+		return false;
 	}
 }
